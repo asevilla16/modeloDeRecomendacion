bb8f55f0820f2791e87a5a242edfc7b6c9107445
Allan <allan.u94l@gmail.com>
2020-12-19 01:34:50 -0600

    HolisxD

--- src/app/recommendation/recommendation.component.ts
+++ src/app/recommendation/recommendation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-recommendation',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecommendationComponent implements OnInit {
 
+  formulario: FormGroup;
   constructor() { }
+
+  
 
   ngOnInit(): void {
   }
